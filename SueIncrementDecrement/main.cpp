8fe76bbb7921e8226f16d5e979482ba9214f5fc6
Chris <cdesch@gmail.com>
2014-11-18 19:48:39 -0500

    Initial Commit

---
+++ SueIncrementDecrement/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SueIncrementDecrement
+//
+//  Created by cj on 11/18/14.
+//  Copyright (c) 2014 KickinEspresso. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
