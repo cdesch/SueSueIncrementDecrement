ea23185e9f55807a94511b6d10f9a9337c867008
Chris <cdesch@gmail.com>
2014-11-18 20:45:30 -0500

    Finished
    Signed-off-by: Chris <cdesch@gmail.com>

--- SueIncrementDecrement/main.cpp
+++ SueIncrementDecrement/main.cpp
@@ -11,5 +11,28 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    
+
+    std::cout << "for i++"<< std::endl;
+    for (int i = 0; i < 10; i++ ){
+        std::cout << i << std::endl;
+    }
+
+    std::cout << "for ++i" << std::endl;
+    for (int i = 0; i < 10; ++i ){
+        std::cout << i << std::endl;
+    }
+    
+    std::cout << "while i++" << std::endl;
+    int i = 0;
+    while (i < 10){
+        std::cout << i++ << std::endl;
+    }
+    
+    std::cout << "while ++i" << std::endl;
+    i = 0;
+    while (i < 10){
+        std::cout << ++i << std::endl;
+    }
     return 0;
 }
